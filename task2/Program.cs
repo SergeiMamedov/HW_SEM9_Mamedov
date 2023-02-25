d424a7afe2ca646bef7d1ff3cb043e61c5c26b8e
Sergei <sergeimamedov9976@gmail.com>
2023-02-26 00:17:07 +0600

    вторая задача

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int M, int N)
+{     
+    if (M == N) 
+    {
+        return M;
+    }                      
+    return N + SumNumbers(M, N - 1);        
+}
+Console.WriteLine($"{SumNumbers(1, 1398)}");
